555ec75b47405f45c1ffb5854f60622e01c175bf
moultamissimane <imanelolirock@gmail.com>
2023-03-16 03:26:18 +0100

    add new version of project

--- server/Routes/ScooterRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const {createScooter, getAllScooters, getScooterById} = require("../controllers/ScooterController");
-
-router.post("/create-scooter", createScooter);
-router.get("/scooters", getAllScooters);
-router.get("/scooter/:id", getScooterById);
-
-module.exports = router;
-
--- server/Routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const Login = require("../controllers/LoginController");
-const Register = require("../controllers/RegisterController");
-
-router.post("/signup", Register);
-router.post("/login", Login);
-
-module.exports = router;
