851dbeaf29981a5f8f54087282629a970a229b08
moultamissimane <imanelolirock@gmail.com>
2023-03-17 12:16:40 +0100

    edit pages in navigation folder

--- client/navigations/Pages.js
+++ client/navigations/Pages.js
@@ -3,19 +3,35 @@ import React from "react";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Landing from "../pages/Landing";
+import Company from "../pages/Company"
+// import MapScreen from "../pages/MapScreen";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import Ionicons from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { View, } from 'react-native';
+
 
 
 const Tab = createMaterialBottomTabNavigator();
 const PageStack = createNativeStackNavigator();
 
 function Pages() {
+    <PageStack.Navigator>
+        <PageStack.Screen
+            name="Landing"
+            component={Landing}
+        />
+    </PageStack.Navigator>
+
     return (
         <Tab.Navigator
             initialRouteName="Home"
-            activeColor="#fff"
-            barStyle={{ backgroundColor: '#000' }}
+            activeColor="#000"
+            barStyle={{
+                backgroundColor: '#fff',
+                borderTopLeftRadius: 30,
+                borderTopRightRadius: 20,
+                height: 50,
+            }}
             screenOptions=
             {({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
@@ -26,32 +42,71 @@ function Pages() {
                             :
                             'home';
                     } else if (route.name === 'Profile') {
-                        iconName = focused ? 'manage_accounts' : 'manage_accounts';
+                        iconName = focused ? 'perm-identity' : 'perm-identity';
+                    } else if (
+                        route.name === 'Company') {
+                        iconName = focused ? 'business' : 'business';
                     }
-                    return <Ionicons name={iconName} size={size} color={color} />;
+                     return (
+                        <View
+                            style={{
+                                width: 25,
+                                height: 25,
+                                borderRadius: 25,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}>
+                            <View>
+                                <Icon
+                                    name={iconName}
+                                    size={25}
+                                    color={focused ? 'black' : '#54b6ff'}
+                                />
+                                {focused && (
+                                    <View
+                                        style={{
+                                            position: 'absolute',
+                                            top: 0,
+                                            right: 0,
+                                            width: 10,
+                                            height: 10,
+                                            borderRadius: 5,
+                                            backgroundColor: '#000',
+                                        }}
+                                    />
+                                )}
+                            </View>
+                        </View>
+                    )
                 },
+                tabBarActiveTintColor: '#54b6ff',
+                tabBarInactiveTintColor: '#54b6ff',
+                tabBarStyle: {
+                    display: 'flex',
+                    height: 45,
+                    backgroundColor: '#fff',
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                },
+                tabBarShowLabel: false,
             })}
-            tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray',
-                labelStyle: { paddingBottom: 10, fontSize: 10 },
-                style: { padding: 10, height: 40 }
-            }}>
+        >
             <Tab.Screen
                 name="Home"
                 component={Home}
-                options={{
-                    headerShown: true
-                }}
+
+            />
+            <Tab.Screen
+                name="Company"
+                component={Company}
+
             />
             <Tab.Screen
                 name="Profile"
                 component={Profile}
-                options={{
-                    headerShown: true
-                }}
+
             />
-           
+
         </Tab.Navigator>
     )
 }
